eb9d04e2adf5fce4370ea24e5794c04342445d93
Michael Redifer <mikeredifer@yahoo.com>
2022-03-09 20:39:57 -0600

    Conversion and styling changes. Replaced Lorem Ipsum and added images

--- PersonalSiteMVC.UI.MVC/App_Start/FilterConfig.cs
+++ PersonalSiteMVC.UI.MVC/App_Start/FilterConfig.cs
@@ -1,7 +1,6 @@
-﻿using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
-namespace PersonalSiteMVC.UI.MVC
+namespace StoreFront.UI.MVC
 {
     public class FilterConfig
     {
--- PersonalSiteMVC.UI.MVC/Controllers/HomeController.cs
+++ PersonalSiteMVC.UI.MVC/Controllers/HomeController.cs
@@ -1,29 +1,74 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
+using PersonalSiteMVC.UI.MVC.Models;
+using System.Net.Mail;
+using System.Net;
+using System.Configuration;
+using System;
 
-namespace PersonalSiteMVC.UI.MVC.Controllers
+namespace StoreFront.UI.MVC.Controllers
 {
     public class HomeController : Controller
     {
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
+        [HttpGet]
+        public ActionResult Portfolio()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
+        [HttpGet]
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Contact(ContactViewModel cvm)
+        {
+            if (ModelState.IsValid)
+            {
+                string body = $"{cvm.Name} sent you this message: <br />" +
+                    $"{cvm.Message} from this email address: {cvm.Email}";
+                MailMessage mm = new MailMessage("administrator@michaelredifer.com", "mikeredifer@yahoo.com", cvm.Subject, body);
 
+                mm.IsBodyHtml = true;
+                mm.Priority = MailPriority.High;
+                mm.ReplyToList.Add(cvm.Email);
+
+                SmtpClient client = new SmtpClient("mail.michaelredifer.com");
+
+                //Client Credentials
+                client.Credentials = new NetworkCredential("administrator@michaelredifer.com", "P@ssw0rd");
+                
+
+                try
+                {
+                    client.Send(mm);
+                }
+                catch (Exception ex)
+                {
+                    ViewBag.CustomerMessage = $"I'm sorry, your request could not be completed at this time. " +
+                        $"Please try again in a moment or use on of the other contact methods.";
+                    return View(cvm);
+                }
+                return View("EmailConfirmation", cvm);
+            }
+            return View(cvm);
+        }
+
+        [HttpGet]
+        public ActionResult Classmates()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Resume()
+        {
             return View();
         }
     }
